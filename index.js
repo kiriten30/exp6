86d06188ff654d08dfb40d3e9c91ef3fe551497b
kiriten30 <kiritenthakur@gmail.com>
2023-02-21 15:05:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+
+let x;  //x=3.14
+// let y = 5;
+// let z=9;
+
+// x = Math.round(x);
+// x = Math.floor(x);
+// x = Math.ceil(x);
+// x = Math.sqrt(x);
+// x = Math.abs(x);
+// maximum = Math.max(x,y,z);
+// minimum = Math.min(x,y,z);
+// x = Math.PI;
+
+
+
+
+// console.log(x);
+
+// find hypotenuse
+// let a;
+// let b;
+// let c;
+// a = window.prompt("enter a");
+// a = Number(a);
+// b = window.prompt("enter b");
+// b = Number(b);
+
+// c= Math.pow(a,2) + Math.pow(b,2);
+// c= Math.sqrt(c);
+// console.log(c);
+
+//advamnced now
+
+// document.getElementById("submitButton").onclick = function(){
+//     a = document.getElementById("aTextBox").value;
+//     a = Number(a);
+//     b = document.getElementById("bTextBox").value;
+//     b = Number(b);
+
+//     c= Math.pow(a,2) + Math.pow(b,2);
+//     c= Math.sqrt(c);
+    
+//     document.getElementById("cLabel").innerHTML = "side c:" +c;
+// }
+
+
+
